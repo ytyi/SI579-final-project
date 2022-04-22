1da4ea6a4ed6eae58b590f0b56e98fb0f6a8d021
unknown <787525729@qq.com>
2022-04-22 16:29:16 -0400

    Updates

---
+++ precache-manifest.96dd323f7161fc03e9217afe13ac2970.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cefee44d9c9e54e4101ae21d6898ac3c",
+    "url": "/SI579-final-project/index.html"
+  },
+  {
+    "revision": "fdc19aa881012aed6d32",
+    "url": "/SI579-final-project/static/css/2.086fabb6.chunk.css"
+  },
+  {
+    "revision": "5e06905ab516059464bf",
+    "url": "/SI579-final-project/static/css/main.721410be.chunk.css"
+  },
+  {
+    "revision": "fdc19aa881012aed6d32",
+    "url": "/SI579-final-project/static/js/2.8d726d37.chunk.js"
+  },
+  {
+    "revision": "3abe76e6723b7a874072f40abdb6851a",
+    "url": "/SI579-final-project/static/js/2.8d726d37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e06905ab516059464bf",
+    "url": "/SI579-final-project/static/js/main.112bf6ce.chunk.js"
+  },
+  {
+    "revision": "5b84f518b0a7ebd4de41",
+    "url": "/SI579-final-project/static/js/runtime-main.5aa0373d.js"
+  }
+]);
